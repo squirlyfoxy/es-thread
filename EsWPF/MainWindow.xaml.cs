0d0bc099569fcc6a3ebac5cb8a8ddfe856721202
squirlyfoxy <public.thesquirlyz97@gmail.com>
2020-12-16 11:30:57 +0100

    Inizio thread

--- EsWPF/MainWindow.xaml.cs
+++ EsWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace EsWPF
 {
@@ -20,9 +22,62 @@ namespace EsWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Thread simulationThread;
+
         public MainWindow()
         {
+            //Thread
+            simulationThread = new Thread(new ThreadStart(SimulationLoop));
+
             InitializeComponent();
+        }
+
+        //Momento precedente
+        private DateTime prevDateTime;
+
+        //FPS
+        public int CurrentFPS { get; private set; }
+        public const double FPS_TO_BLOCK = 60.0; //FPS fissi
+
+        private void SimulationLoop()
+        {
+            double msPrimaDiFrame = 1000.0 / FPS_TO_BLOCK;
+            Debug.WriteLine(msPrimaDiFrame);
+
+            prevDateTime = DateTime.Now;
+
+            Application.Current.Dispatcher.Invoke(new Action(() => 
+            {
+                DateTime ora;
+
+                //Loop simulazione
+                while (simulationThread.IsAlive)
+                {
+                    ora = DateTime.Now;
+
+                    //Controlla se è passato un secondo
+                    if(ora.Second >= prevDateTime.Second + 1)
+                    {
+                        //Stampa gli FPS
+                        Debug.WriteLine("FPS: " + CurrentFPS);
+
+                        CurrentFPS = 0;
+                        prevDateTime = ora;
+                    }
+                    else
+                    {
+                        //Controlla se sono passati "msPrimaDiFrame" millisecondi
+                        if (ora.Millisecond >= prevDateTime.Millisecond + msPrimaDiFrame)
+                        {
+                            //Frame
+
+
+                            CurrentFPS++;
+                            prevDateTime = ora;
+                        }
+                    }
+                }
+            }));
         }
     }
 }
