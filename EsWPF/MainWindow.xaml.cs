6396d18e8d501caf8f41386d29a4bd5d46de4597
squirlyfoxy <public.thesquirlyz97@gmail.com>
2020-12-22 13:17:19 +0100

    Aggiunta funzioine screenshot che verrà usata per l'AI; ricomincio reintroduzione del blocco dell'esecuzione del thread n volte al secondo

--- EsWPF/MainWindow.xaml.cs
+++ EsWPF/MainWindow.xaml.cs
@@ -14,9 +14,27 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
+using System.Drawing;
+using System.Windows.Interop;
 
 namespace EsWPF
 {
+    public class Pixel
+    {
+        public System.Drawing.Color Color { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Pixel(System.Drawing.Color color, int x, int y)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+        }
+
+    }
+
     /// <summary>
     /// Logica di interazione per MainWindow.xaml
     /// </summary>
@@ -31,11 +49,43 @@ namespace EsWPF
         Thread t2;
         Thread finishController;
 
+        public float VelocitaG1 { get; set; } = 5.3f; 
+        public float VelocitaG2 { get; set; } = 4.3f;
+
+        /// <summary>
+        /// Quante volte il thread deve essere eseguito in un secondo
+        /// </summary>
+        public const int VPS = 60;
+
+        /// <summary>
+        /// Raggio screenshot AI
+        /// </summary>
+        private int ray = 100;
+
         private void posG1()
         {
             while(t1.IsAlive)
             {
+                Thread.Sleep(1);
+
                 //MUOVI G1
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Bitmap s = Screenshot(G1);
+
+                    System.Drawing.Color[,] dim = new System.Drawing.Color[s.Width, s.Height];
+                    //Populate px
+                    for(int x = 0; x < s.Width; x++)
+                    {
+                        for (int y = 0; y < s.Height; y++)
+                        {
+                            dim[x, y] = s.GetPixel(x, y);
+                        }
+                    }
+
+                    //
+                }));
             }
         }
 
@@ -43,7 +93,15 @@ namespace EsWPF
         {
             while (t2.IsAlive)
             {
+                Thread.Sleep(1);
+
                 //MUOVI G2
+
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Bitmap s = Screenshot(G2);
+                }));
+
             }
         }
 
@@ -51,6 +109,8 @@ namespace EsWPF
         {
             while (finishController.IsAlive)
             {
+                Thread.Sleep(10);
+
                 //COLLISION DETECTION
             }
         }
@@ -60,6 +120,22 @@ namespace EsWPF
             t1 = new Thread(new ThreadStart(posG1)); t1.Start();
             t2 = new Thread(new ThreadStart(posG2)); t2.Start();
             finishController = new Thread(new ThreadStart(FinishController)); finishController.Start();
+        }
+
+        private Bitmap Screenshot(System.Windows.Controls.Image img)
+        {
+            Bitmap screenshot;
+            System.Drawing.Rectangle rect = new System.Drawing.Rectangle((int)this.Left + (int)img.Margin.Left, (int)this.Top + (int)img.Margin.Top, ray, ray);
+            screenshot = new Bitmap(rect.Width, rect.Height,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            
+            //Get the graphics context
+            using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(screenshot))
+            {
+                g.CopyFromScreen(rect.Left, rect.Top, 0, 0, screenshot.Size, CopyPixelOperation.SourceCopy); ;
+            }
+
+            return screenshot;
         }
     }
 }
